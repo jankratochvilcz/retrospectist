45d63be87283e38380c252260c875690df4f45c9
Jan Kratochvíl <kratochvil.jan@outlook.com>
2018-12-26 12:44:41 +0100

    Adds an incomplete AAD auth

--- src/components/shell/AzureDevOpsUserProfile.js
+++ src/components/shell/AzureDevOpsUserProfile.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
+import AuthenticationContext from 'adal-angular'
+import { TENANT, INSTANCE, POST_LOGOUT_REDIRECT_URI, CLIENT_ID, CACHE_LOCATION } from '../../configuration/azureActiveDirectory';
 
 class AzureDevOpsUserProfile extends Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            user: { }
+        };
+
+        this.props = props;
+    }
+
+    componentDidMount() {
+        // This is just for playing around, all needs to be moved to the redux architecture.
+        var authContext = new AuthenticationContext({
+            instance: INSTANCE,
+            tenant: TENANT,
+            clientId: CLIENT_ID,
+            postLogoutRedirectUri: POST_LOGOUT_REDIRECT_URI,
+            cacheLocation: CACHE_LOCATION
+        });
+
+        authContext.handleWindowCallback();
+        console.log(authContext.getLoginError());
+
+        var user = authContext.getCachedUser();
+
+        this.setState({
+            user: user
+        });
+
+        if(user == null) {
+            authContext.login();
+        }
+
+        console.log(user);
+    }
+
     render() {
         return (
             <div className={this.props.className}>
-                Azure DevOps Login
+                { this.state.user.userName != null ? this.state.user.userName : "Not logged in" }
             </div>
         )
     }
---
+++ src/configuration/azureActiveDirectory.js
@@ -0,0 +1,5 @@
+export const TENANT = "doist.onmicrosoft.com";
+export const INSTANCE = "https://login.microsoftonline.com/";
+export const CLIENT_ID = "afa1f6f6-982b-4b5b-802c-d35d085cf0d9";
+export const POST_LOGOUT_REDIRECT_URI = "http://localhost:3000";
+export const CACHE_LOCATION = "localStorage"
